fc0471faf83dcc73065a52676f27a80e163102a0
Thomas Steimann <thomas.steimann@resolvex.ch>
2014-10-28 07:56:41 +0100

    first commit

---
+++ LoginAndSignUpDemo/AppDelegate.m
@@ -0,0 +1,53 @@
+//
+//  AppDelegate.m
+//  LogInAndSignUpDemo
+//
+//  Created by Mattieu Gamache-Asselin on 6/14/12.
+//  Copyright (c) 2013 Parse. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "DefaultSettingsViewController.h"
+
+@implementation AppDelegate
+
+
+#pragma mark - UIApplicationDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    // ****************************************************************************
+    // Fill in with your Parse and Twitter credentials. Don't forget to add your
+    // Facebook id in Info.plist:
+    // ****************************************************************************
+    [Parse setApplicationId:@"e5tuXqkTMm6Neul8nPqo4DT3sneFtkzEK4TeUCAU" clientKey:@"Ir50yj0NjDqibtLdUPyhlkuyBzPKh4NiYBFmc3AN"];
+    [PFFacebookUtils initializeFacebook];
+    
+    // Set default ACLs
+    PFACL *defaultACL = [PFACL ACL];
+    [defaultACL setPublicReadAccess:YES];
+    [PFACL setDefaultACL:defaultACL withAccessForCurrentUser:YES];
+    
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[DefaultSettingsViewController alloc] init]];
+    self.window.backgroundColor = [UIColor redColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+// Facebook oauth callback
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+    return [PFFacebookUtils handleOpenURL:url];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [PFFacebookUtils handleOpenURL:url];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Handle an interruption during the authorization flow, such as the user clicking the home button.
+    [FBSession.activeSession handleDidBecomeActive];
+}
+
+@end
